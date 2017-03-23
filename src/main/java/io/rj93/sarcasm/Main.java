14d92cfd3f139d69ed07720bc9f3b4872b946652
Richard Jones <contact@richardjones.io>
2017-03-23 19:05:13 +0000

    Added data helper, and slight refactoring

--- src/main/java/io/rj93/sarcasm/Main.java
+++ src/main/java/io/rj93/sarcasm/Main.java
@@ -3,23 +3,24 @@ package io.rj93.sarcasm;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import org.deeplearning4j.models.word2vec.Word2Vec;
+import org.deeplearning4j.plot.BarnesHutTsne;
 import org.deeplearning4j.text.sentenceiterator.SentenceIterator;
 import org.deeplearning4j.text.tokenization.tokenizer.preprocessor.CommonPreprocessor;
 import org.deeplearning4j.text.tokenization.tokenizerfactory.DefaultTokenizerFactory;
 import org.deeplearning4j.text.tokenization.tokenizerfactory.TokenizerFactory;
 
-import io.rj93.sarcasm.conf.Configuration;
+import io.rj93.sarcasm.data.DataHelper;
 
 public class Main {
 	
 	public static void main(String[] args) throws IOException{
-		String fileNonSarcyPath = Configuration.getDataDir() + "preprocessed_data/2015/RC_2015-01-non-sarcy.json";
-		String fileSarcyPath = Configuration.getDataDir() + "preprocessed_data/2015/RC_2015-01-sarcy.json";
+		String fileNonSarcyPath = DataHelper.DATA_DIR + "preprocessed_data/2015/RC_2015-01-non-sarcy.json";
+		String fileSarcyPath = DataHelper.DATA_DIR + "preprocessed_data/2015/RC_2015-01-sarcy.json";
 		List<File> files = Arrays.asList(new File(fileNonSarcyPath), new File(fileSarcyPath));
-		
 		
 		SentenceIterator iter = new MultiFileLineSentenceIterator(files);
 		
@@ -37,12 +38,27 @@ public class Main {
                 .tokenizerFactory(t)
                 .build();
 		
-		System.out.println("Fitting model");
+		System.out.print("Fitting model... ");
+		long start = System.currentTimeMillis();
 		vec.fit();
+		long timeTaken = (System.currentTimeMillis() - start) / 1000;
+		System.out.println("Finished in " + timeTaken + " seconds");
 		
-		System.out.println("Finished");
-
-
+		System.out.println("Closest Words: ");
+        Collection<String> lst = vec.wordsNearest("like", 10);
+        System.out.println(lst);
+        
+        BarnesHutTsne tsne = new BarnesHutTsne.Builder()
+                .setMaxIter(1000)
+                .stopLyingIteration(250)
+                .learningRate(500)
+                .useAdaGrad(false)
+                .theta(0.5)
+                .setMomentum(0.5)
+                .normalize(true)
+                .build();
+        vec.lookupTable().plotVocab(tsne, 3, new File("test.csv"));
+        
 	}
 	
 	
---
+++ src/main/java/io/rj93/sarcasm/data/DataHelper.java
@@ -0,0 +1,45 @@
+package io.rj93.sarcasm.data;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataHelper {
+	
+	public final static String DATA_DIR = getDataDir();
+	public final static String REDDIT_DATA_DIR = DATA_DIR + "reddit_data";
+	public final static String SORTED_DATA_DIR = DATA_DIR + "output_data";
+	public final static String PREPROCESSED_DATA_DIR = DATA_DIR + "preprocessed_data";
+	
+	private static String getDataDir(){
+		String dir = null;
+		if (System.getProperty("os.name").contains("Windows")){
+			dir = "E:/";
+		} else {
+			dir = "/Users/richardjones/Documents/";
+		}
+		dir += "Project/data/";
+		return dir;
+	}
+	
+	public static List<File> getFilesFromDir(File dir) throws FileNotFoundException{
+		return getFilesFromDir(dir, false);
+	}
+	
+	public static List<File> getFilesFromDir(File dir, boolean recursive) throws FileNotFoundException{
+		
+		if (!dir.exists() || !dir.isDirectory())
+			throw new FileNotFoundException("Directory '" + dir.getAbsolutePath() + "' either does not exist, or is not a directory");
+		
+		List<File> files = new ArrayList<File>();
+		for (File f : dir.listFiles()) {
+			if (f.isFile())
+				files.add(f);
+			else if (recursive)
+				files.addAll(getFilesFromDir(f, true));
+		}
+		
+		return files;
+	}
+}
