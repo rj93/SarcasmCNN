3998c56f12bd1c61896eee6affb99c21b95e8731
Richard Jones <contact@richardjones.io>
2017-04-13 22:06:14 +0100

    Forgot to add TextCNN...

--- src/main/java/io/rj93/sarcasm/cnn/TextCNN.java
+++ src/main/java/io/rj93/sarcasm/cnn/TextCNN.java
@@ -4,35 +4,35 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Random;
 
+import org.apache.commons.io.FileUtils;
+import org.deeplearning4j.eval.Evaluation;
 import org.deeplearning4j.iterator.CnnSentenceDataSetIterator;
 import org.deeplearning4j.iterator.LabeledSentenceProvider;
-import org.deeplearning4j.iterator.provider.FileLabeledSentenceProvider;
+import org.deeplearning4j.iterator.CnnSentenceDataSetIterator.UnknownWordHandling;
+import org.deeplearning4j.iterator.provider.CollectionLabeledSentenceProvider;
 import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
 import org.deeplearning4j.models.word2vec.Word2Vec;
-import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.ComputationGraphConfiguration;
 import org.deeplearning4j.nn.conf.ConvolutionMode;
-import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.conf.Updater;
-import org.deeplearning4j.nn.conf.inputs.InputType;
+import org.deeplearning4j.nn.conf.graph.MergeVertex;
 import org.deeplearning4j.nn.conf.layers.ConvolutionLayer;
-import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.GlobalPoolingLayer;
 import org.deeplearning4j.nn.conf.layers.OutputLayer;
-import org.deeplearning4j.nn.conf.layers.SubsamplingLayer;
-import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.conf.layers.PoolingType;
+import org.deeplearning4j.nn.graph.ComputationGraph;
 import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.util.ModelSerializer;
 import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
 
 import io.rj93.sarcasm.data.DataHelper;
-import io.rj93.sarcasm.iterators.TextDataSetIterator;
+import io.rj93.sarcasm.filters.TestFileFilter;
+import io.rj93.sarcasm.filters.TrainFileFilter;
 
 public class TextCNN {
 	
@@ -40,106 +40,159 @@ public class TextCNN {
 	private int nOutputs;
 	private int batchSize;
 	private int nEpochs;
-	private int iterations;
-	private int seed = 12345;
-	private MultiLayerConfiguration conf;
-	private MultiLayerNetwork model;
+	private int iterations = 1;
+	private int seed;
+	private Word2Vec embedding;
+	private int vectorSize;
+	private int maxSetenceLength;
+	private int cnnLayerFeatureMaps = 10;
+	private ComputationGraphConfiguration conf;
+	private ComputationGraph model;
 	
-	public TextCNN(int nChannels, int nOutputs, int batchSize, int nEpochs, int iterations){
-		this(nChannels, nOutputs, batchSize, nEpochs, iterations, 12345);
+	public TextCNN(int nChannels, int nOutputs, int batchSize, int nEpochs, Word2Vec embedding, int maxSetenceLength){
+		this(nChannels, nOutputs, batchSize, nEpochs, embedding, maxSetenceLength, 12345);
 	}
 	
-	public TextCNN(int nChannels, int nOutputs, int batchSize, int nEpochs, int iterations, int seed){
+	public TextCNN(int nChannels, int nOutputs, int batchSize, int nEpochs, Word2Vec embedding, int maxSetenceLength, int seed){
 		this.nChannels = nChannels;
 		this.nOutputs = nOutputs;
 		this.batchSize = batchSize;
 		this.nEpochs = nEpochs;
-		this.iterations = iterations;
+		
+		if (embedding.getUNK() == null)
+			embedding.setUNK("UNK");
+		this.embedding = embedding;
+		
+		this.vectorSize = embedding.getLayerSize();
+		this.maxSetenceLength = maxSetenceLength;
 		this.seed = seed;
 		this.conf = getConf();
-		model = new MultiLayerNetwork(conf);
+		model = new ComputationGraph(conf);
         model.init();
 	}
 	
-	private MultiLayerConfiguration getConf(){
-        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
-                .seed(seed)
-                .iterations(iterations) // Training iterations as above
-                .convolutionMode(ConvolutionMode.Same) 
-                .regularization(true).l2(0.0005)
-                .learningRate(.01)//.biasLearningRate(0.02)
-                //.learningRateDecayPolicy(LearningRatePolicy.Inverse).lrPolicyDecayRate(0.001).lrPolicyPower(0.75)
-                .weightInit(WeightInit.XAVIER)
-                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
-                .updater(Updater.NESTEROVS).momentum(0.9)
-                .list()
-                .layer(0, new ConvolutionLayer.Builder(5, 5)
-                        //nIn and nOut specify depth. nIn here is the nChannels and nOut is the number of filters to be applied
-                        .nIn(nChannels)
-                        .stride(1, 1)
-                        .nOut(20)
-                        .activation(Activation.IDENTITY)
-                        .build())
-                .layer(1, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
-                        .kernelSize(2,2)
-                        .stride(2,2)
-                        .build())
-                .layer(2, new ConvolutionLayer.Builder(5, 5)
-                        //Note that nIn need not be specified in later layers
-                		.nIn(nChannels)
-                        .stride(1, 1)
-                        .nOut(50)
-                        .activation(Activation.IDENTITY)
-                        .build())
-                .layer(3, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
-                        .kernelSize(2,2)
-                        .stride(2,2)
-                        .build())
-                .layer(4, new DenseLayer.Builder().activation(Activation.RELU)
-                		.nIn(nChannels)
-                        .nOut(500).build())
-                .layer(5, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
-                		.nIn(nChannels)
-                        .nOut(nOutputs)
-                        .activation(Activation.SOFTMAX)
-                        .build())
-//                .setInputType(InputType.convolutional(28,28,1))
-                .backprop(true)
-                .pretrain(false)
-                .build();
-        return conf;
+	private ComputationGraphConfiguration getConf(){
+		ComputationGraphConfiguration conf = new NeuralNetConfiguration.Builder()
+	            .weightInit(WeightInit.RELU)
+	            .activation(Activation.LEAKYRELU)
+	            .updater(Updater.ADAM)
+	            .convolutionMode(ConvolutionMode.Same)
+	            .seed(seed)
+	            .iterations(iterations)
+	            .regularization(true).l2(0.0001)
+	            .learningRate(0.01)
+	            .graphBuilder()
+	            .addInputs("input")
+	            .addLayer("cnn3", new ConvolutionLayer.Builder()
+	                .kernelSize(3,vectorSize)
+	                .stride(1,vectorSize)
+	                .nIn(nChannels)
+	                .nOut(cnnLayerFeatureMaps)
+//	                .name("cnn3")
+//	                .adamMeanDecay(0.999)
+//	                .adamVarDecay(0.9)
+	                .build(), "input")
+	            .addLayer("cnn4", new ConvolutionLayer.Builder()
+	                .kernelSize(4,vectorSize)
+	                .stride(1,vectorSize)
+	                .nIn(nChannels)
+	                .nOut(cnnLayerFeatureMaps)
+	                .build(), "input")
+	            .addLayer("cnn5", new ConvolutionLayer.Builder()
+	                .kernelSize(5,vectorSize)
+	                .stride(1,vectorSize)
+	                .nIn(nChannels)
+	                .nOut(cnnLayerFeatureMaps)
+	                .build(), "input")
+	            .addVertex("merge", new MergeVertex(), "cnn3", "cnn4", "cnn5")
+	            .addLayer("globalPool", new GlobalPoolingLayer.Builder()
+	                .poolingType(PoolingType.MAX)
+	                .build(), "merge")
+	            .addLayer("out", new OutputLayer.Builder()
+	                .lossFunction(LossFunctions.LossFunction.MCXENT)
+	                .activation(Activation.SOFTMAX)
+	                .nIn(3*cnnLayerFeatureMaps)
+	                .nOut(nOutputs)
+	                .build(), "globalPool")
+	            .setOutputs("out")
+	            .build();
+		return conf;
 	}
 	
-	public void train(Word2Vec embedding, List<File> files) throws IOException{
-		
-		List<File> positiveFiles = new ArrayList<File>();
-		List<File> negativeFiles = new ArrayList<File>();
+	private DataSetIterator getDataSetIterator(List<File> files) throws FileNotFoundException{
+
+		List<String> sentences = new ArrayList<String>();
+		List<String> labels = new ArrayList<String>();
+		int posCount = 0;
+		int negCount = 0;
 		for (File f : files){
-			if (f.getName().contains("non-sarcy")){
-				negativeFiles.add(f);
-			} else {
-				positiveFiles.add(f);
+			
+			List<String> s;
+			try {
+				s = FileUtils.readLines(f, "UTF-8");
+				sentences.addAll(s);
+				
+				String label;
+				if (f.getAbsolutePath().contains("pos")){
+					label = "positive";
+					posCount += s.size();
+				} else {
+					label = "negative";
+					negCount += s.size();
+				}
+				for (int i = 0; i < s.size(); i++){
+					labels.add(label);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 		}
 		
-		Random r = new Random(100);
-		Map<String,List<File>> filesMap = new HashMap<>();
-        filesMap.put("Positive", positiveFiles);
-        filesMap.put("Negative", negativeFiles);
+		System.out.println("No. positive: " + posCount + ", No. negative: " + negCount);
+		LabeledSentenceProvider sentenceProvider = new CollectionLabeledSentenceProvider(sentences, labels);
 		
-		LabeledSentenceProvider sentenceProvider = new FileLabeledSentenceProvider(filesMap, r);
 		
 		DataSetIterator iter = new CnnSentenceDataSetIterator.Builder()
-				.sentenceProvider(sentenceProvider)
-				.wordVectors(embedding)
-				.minibatchSize(batchSize)
-				.maxSentenceLength(100)
-				.useNormalizedWordVectors(false)
-				.build();
+        		.sentenceProvider(sentenceProvider)
+                .wordVectors(embedding)
+                .minibatchSize(batchSize)
+                .maxSentenceLength(maxSetenceLength)
+                .useNormalizedWordVectors(false)
+                .unknownWordHandling(UnknownWordHandling.UseUnknownVector)
+                .build();
+		
+        return iter;
+    }
+	
+	public void train(List<File> trainFiles, List<File> testFiles) throws IOException {
 		
 		System.out.println("Training Model...");
-		model.fit(iter);
+		for (int i = 0; i < nEpochs; i++){
+			DataSetIterator trainIter = getDataSetIterator(trainFiles);
+			DataSetIterator testIter = getDataSetIterator(testFiles);
+			
+			System.out.print("Starting epoch " + i + "... ");
+			long start = System.nanoTime();
+			model.fit(trainIter);
+			long diff = System.nanoTime() - start;
+			System.out.print("complete in " + diff / 1000000 + " ms. Starting evaluation... ");
+			
+			start = System.nanoTime();
+            Evaluation evaluation = model.evaluate(testIter);
+            diff = System.nanoTime() - start;
+            System.out.println("complete in: " + diff / (1000 * 1000) + "ms");
+            
+            System.out.println(evaluation.stats());
+		}
 		System.out.println("Training Complete");
+	}
+	
+	public void save(File file) throws IOException{
+		save(file, false);
+	}
+	
+	public void save(File file, boolean saveUpdater) throws IOException{
+		ModelSerializer.writeModel(model, file, saveUpdater);
 	}
 	
 	public void test(){
@@ -151,14 +204,31 @@ public class TextCNN {
 	}
 	
 	public static void main(String[] args) throws IOException {
+		File dir = new File(DataHelper.PREPROCESSED_DATA_DIR + "/2015-quick");
+		
 		// CnnSentenceDataSetIterator
 //		Word2Vec embedding = WordVectorSerializer.readWord2VecModel(DataHelper.WORD2VEC_DIR + "all-preprocessed-300.emb");
 		System.out.println("Reading word embedding");
 		Word2Vec embedding = WordVectorSerializer.readWord2VecModel(DataHelper.GOOGLE_NEWS_WORD2VEC);
 		System.out.println("Complete");
 		
-		TextCNN cnn = new TextCNN(1, 2, 64, 10, 10);
-		cnn.train(embedding, DataHelper.getFilesFromDir(DataHelper.PREPROCESSED_DATA_DIR, true));
+		List<File> trainFiles = DataHelper.getFilesFromDir(dir, new TrainFileFilter(), true);
+		List<File> testFiles = DataHelper.getFilesFromDir(dir, new TestFileFilter(), true);
+		
+		int channels = 1;
+		int outputs = 2;
+		int batchSize = 64;
+		int epochs = 5;
+		int iterations = 1;
+		int maxSentenceLength = 200;
+		
+		TextCNN cnn = new TextCNN(channels, outputs, batchSize, epochs, embedding, maxSentenceLength);
+		long start = System.currentTimeMillis();
+		cnn.train(trainFiles, testFiles);
+		long end = System.currentTimeMillis();
+		System.out.println("Time taken: " + (end - start));
+		
+
 		
 	}
 }
