6ecdf9366cc271b853052e525e28575f1c0f40a6
Richard Jones <contact@richardjones.io>
2017-04-20 22:19:51 +0100

    base website created

--- src/main/java/io/rj93/sarcasm/filters/TestFileFilter.java
+++
@@ -1,9 +0,0 @@
-package io.rj93.sarcasm.filters;
-
-public class TestFileFilter extends TrainOrTestFileFilter {
-	
-	public TestFileFilter(int parentIndex){
-		super(false, parentIndex);
-	}
-	
-}
--- src/main/java/io/rj93/sarcasm/filters/TrainFileFilter.java
+++
@@ -1,9 +0,0 @@
-package io.rj93.sarcasm.filters;
-
-public class TrainFileFilter extends TrainOrTestFileFilter {
-	
-	public TrainFileFilter(int parentIndex){
-		super(true, parentIndex);
-	}
-	
-}
