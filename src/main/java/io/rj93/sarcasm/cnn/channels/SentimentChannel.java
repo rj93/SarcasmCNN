92e2530b25e7aaa45127a1157b548212c7e5a8af
Richard Jones <contact@richardjones.io>
2017-04-29 17:12:20 +0100

    Added SentiWordNet

--- src/main/java/io/rj93/sarcasm/cnn/channels/SentimentChannel.java
+++ src/main/java/io/rj93/sarcasm/cnn/channels/SentimentChannel.java
@@ -1,8 +1,13 @@
 package io.rj93.sarcasm.cnn.channels;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.deeplearning4j.text.tokenization.tokenizer.Tokenizer;
 import org.deeplearning4j.text.tokenization.tokenizerfactory.DefaultTokenizerFactory;
@@ -14,6 +19,9 @@ import org.nd4j.linalg.indexing.INDArrayIndex;
 import org.nd4j.linalg.indexing.NDArrayIndex;
 
 public class SentimentChannel extends Channel {
+	
+    private static final String pathToSWN = "src/main/resources/SentiWordNet_3.0.0_20130122.txt";
+    private static Map<String, Double> dictionay = loadDictionary();
 	
 	private int nParts;
 	private TokenizerFactory tokenizerFactory = new DefaultTokenizerFactory();
@@ -39,8 +47,8 @@ public class SentimentChannel extends Channel {
 		int[] featureShape = new int[4];
         featureShape[0] = sentences.size();
         featureShape[1] = 1;
-        featureShape[2] = 1;
-        featureShape[3] = nParts;
+        featureShape[2] = nParts;
+        featureShape[3] = 1;
 		INDArray features = Nd4j.create(featureShape);
 		// System.out.println("features: " + features);
 		for (int i = 0; i < sentences.size(); i++) {
@@ -92,11 +100,14 @@ public class SentimentChannel extends Channel {
 	private double getSentimentScore(String word){
 		// TODO
 		double score = 0;
-		if (word.equals("good")){
-			score = 1;
-		} else if (word.equals("bad")){
-			score = -1;
-		}
+	    if(dictionay.get(word+"#n") != null)
+	    	score = dictionay.get(word+"#n") + score;
+	    if(dictionay.get(word+"#a") != null)
+	    	score = dictionay.get(word+"#a") + score;
+	    if(dictionay.get(word+"#r") != null)
+	    	score = dictionay.get(word+"#r") + score;
+	    if(dictionay.get(word+"#v") != null)
+	    	score = dictionay.get(word+"#v") + score;
 		return score;
 	}
 	
@@ -110,6 +121,102 @@ public class SentimentChannel extends Channel {
 		return tokens;
 	}
 
+	private static Map<String, Double> loadDictionary(){
+		Map<String, Double> dict = new HashMap<String, Double>();
+
+		// From String to list of doubles.
+		HashMap<String, HashMap<Integer, Double>> tempDictionary = new HashMap<String, HashMap<Integer, Double>>();
+
+		BufferedReader csv = null;
+		try {
+			csv = new BufferedReader(new FileReader(pathToSWN));
+			int lineNumber = 0;
+
+			String line;
+			while ((line = csv.readLine()) != null) {
+				lineNumber++;
+
+				// If it's a comment, skip this line.
+				if (!line.trim().startsWith("#")) {
+					// We use tab separation
+					String[] data = line.split("\t");
+					String wordTypeMarker = data[0];
+
+					// Example line:
+					// POS ID PosS NegS SynsetTerm#sensenumber Desc
+					// a 00009618 0.5 0.25 spartan#4 austere#3 ascetical#2
+					// ascetic#2 practicing great self-denial;...etc
+
+					// Is it a valid line? Otherwise, through exception.
+					if (data.length != 6) {
+						throw new IllegalArgumentException(
+								"Incorrect tabulation format in file, line: "
+										+ lineNumber);
+					}
+
+					// Calculate synset score as score = PosS - NegS
+					Double synsetScore = Double.parseDouble(data[2])
+							- Double.parseDouble(data[3]);
+
+					// Get all Synset terms
+					String[] synTermsSplit = data[4].split(" ");
+
+					// Go through all terms of current synset.
+					for (String synTermSplit : synTermsSplit) {
+						// Get synterm and synterm rank
+						String[] synTermAndRank = synTermSplit.split("#");
+						String synTerm = synTermAndRank[0] + "#"
+								+ wordTypeMarker;
+
+						int synTermRank = Integer.parseInt(synTermAndRank[1]);
+						// What we get here is a map of the type:
+						// term -> {score of synset#1, score of synset#2...}
+
+						// Add map to term if it doesn't have one
+						if (!tempDictionary.containsKey(synTerm)) {
+							tempDictionary.put(synTerm,
+									new HashMap<Integer, Double>());
+						}
+
+						// Add synset link to synterm
+						tempDictionary.get(synTerm).put(synTermRank,
+								synsetScore);
+					}
+				}
+			}
+
+			// Go through all the terms.
+			for (Map.Entry<String, HashMap<Integer, Double>> entry : tempDictionary
+					.entrySet()) {
+				String word = entry.getKey();
+				Map<Integer, Double> synSetScoreMap = entry.getValue();
+
+				// Calculate weighted average. Weigh the synsets according to
+				// their rank.
+				// Score= 1/2*first + 1/3*second + 1/4*third ..... etc.
+				// Sum = 1/1 + 1/2 + 1/3 ...
+				double score = 0.0;
+				double sum = 0.0;
+				for (Map.Entry<Integer, Double> setScore : synSetScoreMap
+						.entrySet()) {
+					score += setScore.getValue() / (double) setScore.getKey();
+					sum += 1.0 / (double) setScore.getKey();
+				}
+				score /= sum;
+
+				dict.put(word, score);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (csv != null) {
+				try { csv.close();} catch (IOException e) {}
+			}
+		}
+		return dict;
+	}
+
+	
 	@Override
 	public JSONObject getConfig() {
 		// TODO Auto-generated method stub
